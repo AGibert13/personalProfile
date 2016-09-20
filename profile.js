8ac572d74b89f83cb04703ccbbc1bbe803203cb5
Gibert <Adrien.Gibert@carters.com>
2016-09-20 15:22:51 -0400

    i

---
+++ profile.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('#welcome').fadeIn(1000).animate({fontSize:"2.5em"}, 1000, "linear").dequeue();
+	$('#slide').click(function(){
+		if ($(this).is(':checked')){
+			$('#theme').attr('href','spidey.css')
+		}
+		else{
+			$('#theme').attr('href', 'style.css')
+		}
+
+	});
+	$('#aboutButton, #aboutLink').click(function(){
+		fade('#homeNav, #code, #projects, #welcome','#aboutHead, #about, #headerNav')	
+	})
+	$('#codeButton, #codeLink').click(function(){
+		fade('#homeNav, #about, #projects, #welcome','#codeHead, #code, #headerNav')	
+	})
+	$('#projectButton, #projectLink').click(function(){	
+		fade('#homeNav, #about, #code, #welcome','#projectHead, #projects, #headerNav')
+	})
+
+	function fade(outId, inId){
+		$(outId).fadeOut(function(){
+				$(inId).fadeIn(2000);
+		});
+	}
+});
