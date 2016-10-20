2c3c72926cc595ba02e3b96db8b4000fbc516e27
Gibert <Adrien.Gibert@carters.com>
2016-10-20 14:47:16 -0400

    commit

--- profile.js
+++ profile.js
@@ -1,36 +1,69 @@
 $(document).ready(function(){
-	$('#welcome').fadeIn(1000).animate({fontSize:"2.5em"}, 1000, "linear").dequeue();
 	$('#slide').click(function(){
-		if ($(this).is(':checked')){
-			$('#theme').attr('href','spidey.css')
-		}
-		else{
-			$('#theme').attr('href', 'style.css')
-		}
+			if ($(this).is(':checked')){
+				$('#theme').attr('href','spidey.css')
+			}
+			else{
+				$('#theme').attr('href', 'style.css')
+			}
 
-	});
-	$('#aboutButton, #aboutLink').click(function(){
-		fade('#homeNav, #code, #projects, #welcome','#aboutHead, #about, #headerNav')	
-	})
-	$('#codeButton, #codeLink').click(function(){
-		fade('#homeNav, #about, #projects, #welcome','#codeHead, #code, #headerNav')	
-	})
-	$('#projectButton, #projectLink').click(function(){	
-		fade('#homeNav, #about, #code, #welcome','#projectHead, #projects, #headerNav')
-	})
+		});
 
 	function fade(outId, inId){
-		$(outId).fadeOut(function(){
-				$(inId).fadeIn(2000);
-		});
+			$(outId).fadeOut(function(){
+					$(inId).fadeIn(2000);
+			});
+		}
+		$('.website').click(function(){
+			var source = $(this).attr('data-link');
+			$('#websiteDisplay').attr('src',source);
+			$('#frame').show();
+		})
+		$('#close').click(function(){
+			$('#frame').hide();
+			$('#websiteDisplay').removeAttr('src');
+		})
+	function switchView(){
+		if (screen.height < 801) {
+			if ($('#headerNav')) {
+				$('#headerNav').attr({'id':'mobileNav'})
+				$('#welcome').css({'display':'none'})
+			}
+			$('#aboutButton, #aboutLink').click(function(){
+				fade('#homeNav, #code, #projects','#aboutHead, #about, #hambugerMenu')	
+			})
+			$('#codeButton, #codeLink').click(function(){
+				fade('#homeNav, #about, #projects','#codeHead, #code, #hambugerMenu')	
+			})
+			$('#projectButton, #projectLink').click(function(){	
+				fade('#homeNav, #about, #code','#projectHead, #projects, #hambugerMenu')
+			})
+			$('#hambugerMenu').click(function(){
+				$('#mobileNav').css({"width": "40%", 'height': '100%', 'display':'block'});
+			})
+			$('#closeNav').click(function(){
+				$('#mobileNav').removeAttr('style');
+			})
+
+		}
+	
+		else{
+			if ($('mobileNav')) {
+				$('#mobileNav').attr({'id':'headerNav'})
+			}
+			$('#welcome').fadeIn(1000).animate({fontSize:"2.5em"}, 1000, "linear").dequeue();
+			$('#aboutButton, #aboutLink').click(function(){
+				fade('#homeNav, #code, #projects, #welcome','#aboutHead, #about, #headerNav')	
+			})
+			$('#codeButton, #codeLink').click(function(){
+				fade('#homeNav, #about, #projects, #welcome','#codeHead, #code, #headerNav')	
+			})
+			$('#projectButton, #projectLink').click(function(){	
+				fade('#homeNav, #about, #code, #welcome','#projectHead, #projects, #headerNav')
+			})
+		}
 	}
-	$('.website').click(function(){
-		var source = $(this).attr('data-link');
-		$('#websiteDisplay').attr('src',source);
-		$('#frame').show();
-	})
-	$('#close').click(function(){
-		$('#frame').hide();
-		$('#websiteDisplay').removeAttr('src');
+	$(window).resize(function(){
+		switchView();
 	})
 });
